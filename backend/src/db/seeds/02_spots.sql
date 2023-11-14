88bb287fd65627a41f565a18c56c17a76c84e97b
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-14 15:01:10 -0800

    Add NOT NULL to all primary keys

---
+++ backend/src/db/seeds/02_spots.sql
@@ -0,0 +1,7 @@
+INSERT INTO SPOTS(lat, lng, name, city, province, country)
+VALUES
+  ('48.424158647072495', '-123.41913232109277', 'Saxe Point', 'Victoria', 'BC', 'Canada'),
+  ('48.403536', '-123.349094', 'Clover Point', 'Victoria', 'BC', 'Canada'),
+  ('48.413711465829415', '-123.39385287893029', 'Breakwater', 'Victoria', 'BC', 'Canada'),
+  ('48.49393121920025', '-123.3450197670181', 'Mount Douglas', 'Victoria', 'BC', 'Canada'),
+  ('48.41149255145647', '-123.34585496002177', 'Moss Rock', 'Victoria', 'BC', 'Canada'),
