f0a935250a0b82c1b4d9ddc7fd3e70cab9b6d1a8
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-15 15:37:17 -0800

    update classnames

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -6,8 +6,8 @@ export default function TopNavBar() {
     <img className='nav__logo' alt='logo' src={process.env.PUBLIC_URL + '/logo.png'}></img>
 
     <div>
-      <button className='login__btn'>Log In</button>
-      <button className='register__btn'>Register</button>
+      <button className='nav__login-btn'>Log In</button>
+      <button className='nav__register-btn'>Register</button>
     </div>
 
     </nav>
