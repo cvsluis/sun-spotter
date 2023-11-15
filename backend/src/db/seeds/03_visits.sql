3b457a84bb42d48855902bbdd301506eb5b261be
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-14 16:39:44 -0800

    Refactor image path

--- backend/src/db/seeds/03_visits.sql
+++ backend/src/db/seeds/03_visits.sql
@@ -1,11 +1,11 @@
 INSERT INTO VISITS(user_id, spot_id, name, image_url, rating, description, time_stamp, created_at)
 VALUES
-  (1, 8, 'Perfect Sunset!', './../public/images/tolmie.jpg', 5, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
-  (1, 5, 'Mossy Glory', './../public/images/mossrock.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
-  (1, 10, 'Summer Eve', './../public/images/NEEDSPIC', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
-  (2, 1, 'Great Views!', './../public/images/saxe.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
-  (2, 5, 'Moss Rocks!', './../public/images/NEEDSPIC', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
-  (2, 7, 'Ocean Sunset', './../public/images/harling_point.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
-  (3, 3, 'Cloudy Skies', './../public/images/breakwater.jpg_large', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
+  (1, 8, 'Perfect Sunset!', 'public/images/tolmie.jpg', 5, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
+  (1, 5, 'Mossy Glory', 'public/images/mossrock.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
+  (1, 10, 'Summer Eve', 'public/images/NEEDSPIC', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
+  (2, 1, 'Great Views!', 'public/images/saxe.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
+  (2, 5, 'Moss Rocks!', 'public/images/NEEDSPIC', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
+  (2, 7, 'Ocean Sunset', 'public/images/harling_point.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
+  (3, 3, 'Cloudy Skies', 'public/images/breakwater.jpg', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
   (3, ),
   ();
