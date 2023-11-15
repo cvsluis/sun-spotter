fa3af3f4931d54071efb89dd5851bf8e526bed66
Janay Ma <janay.ma7@gmail.com>
2023-11-15 13:20:50 -0800

    add api call to 1 spot

--- frontend/src/routes/OneSpot.jsx
+++ frontend/src/routes/OneSpot.jsx
@@ -1,11 +1,36 @@
+// first let's make sure we can get one spot's data, say with id: 1
+
+
+import React, { useEffect, useState } from 'react';
+
+
 export default function OneSpot () {
+
+  //fetch spot data:
+  const [ spot, setSpot ] = useState();
+  
+  //fetch spot data. for now console log result
+  useEffect(() => {
+    fetch('http://localhost:8080/api/spot/1')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <>
-    <div className="spot-container">
+    <div className="one-spot">
+      you are on the one spot page
 
-      
     </div>
 
     </>
   );
-}
+};
