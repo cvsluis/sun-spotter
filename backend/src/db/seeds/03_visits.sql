7b398b969f02f5924bc2932c6e2d3e0ab0c9ae06
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-15 08:51:50 -0800

    Add VISITS seeds.

--- backend/src/db/seeds/03_visits.sql
+++ backend/src/db/seeds/03_visits.sql
@@ -1,11 +1,18 @@
 INSERT INTO VISITS(user_id, spot_id, name, image_url, rating, description, time_stamp, created_at)
 VALUES
-  (1, 8, 'Perfect Sunset!', 'public/images/tolmie.jpg', 5, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
-  (1, 5, 'Mossy Glory', 'public/images/mossrock.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
-  (1, 10, 'Summer Eve', 'public/images/NEEDSPIC', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
-  (2, 1, 'Great Views!', 'public/images/saxe.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
-  (2, 5, 'Moss Rocks!', 'public/images/moss_rock.jpg', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
-  (2, 7, 'Ocean Sunset', 'public/images/harling_point.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
-  (3, 3, 'Cloudy Skies', 'public/images/breakwater.jpg', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
-  (3, 5, 'Amazing Victoria Views!', 'public/images/mossRock.jpg', 5, 'Best sunset in Vic!', '2023-03-9 16:30:00', DEFAULT),
-  (3, 2, 'Clover Sunset', 'public/images/clover_point.jpg', 4, 'Always beautiful, sometimes too windy!!', '2023-06-20 18:30:00', DEFAULT);
+  (1, 8, 'Perfect Sunset!', 'public/images/13.jpg', 5, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
+  (1, 5, 'Mossy Glory', 'public/images/6.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
+  (1, 10, 'Summer Eve', 'public/images/5.jpg', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
+  (2, 1, 'Great Views!', 'public/images/3.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
+  (2, 5, 'Moss Rocks!', 'public/images/9.jpg', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
+  (2, 7, 'Ocean Sunset', 'public/images/12.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
+  (3, 3, 'Cloudy Skies', 'public/images/11.jpg', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
+  (3, 5, 'Amazing Victoria Views!', 'public/images/16.jpg', 5, 'Best sunset in Vic!', '2023-03-9 16:30:00', DEFAULT),
+  (3, 2, 'Clover Sunset', 'public/images/18.jpg', 4, 'Always beautiful, sometimes too windy!!', '2023-06-20 18:30:00', DEFAULT),
+  (1, 10, 'The Moon!', 'public/images/15.jpg', 4, 'Beautiful and pink!', '2023-07-26 21:30:00', DEFAULT),
+  (2, 7, 'Great Light Today :)', 'public/images/17.jpg', 4, 'Gorgeous!', '2023-05-14 19:30:00', DEFAULT),
+  (3, 10, 'Evening Sun', 'public/images/12.jpg', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
+  (1, 1, 'Beautiful Pink Skies!', 'public/images/7.jpg', 4, 'Pink Pink PInk!', '2023-05-14 19:30:00', DEFAULT),
+  (3, 10, 'Sunset Today', 'public/images/13.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT),
+  (2, 10, 'Sunbeams', 'public/images/10.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT),
+  (3, 5, 'Sunset Today', 'public/images/19.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT);
