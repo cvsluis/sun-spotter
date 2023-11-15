8c331d6689057097bd8546f0348e60bcac4a266f
Janay Ma <janay.ma7@gmail.com>
2023-11-15 12:12:30 -0800

    refactor Root to display child views

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+
 export default function Root () {
-  return <div>
-    A Homepage!
+  return <div class ="root-layout">
+    <header>
+      page header here
+    </header>
+
+    <main>
+      <Outlet />
+    </main>
+
+
   </div>
 }
