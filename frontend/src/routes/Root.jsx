1b6b96202825cd410bb424f95f74587ce21fa377
Janay Ma <janay.ma7@gmail.com>
2023-11-15 12:16:18 -0800

    clean up code

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 export default function Root () {
-  return <div className ="root-layout">
-    <header>
-      page header here
-    </header>
-
-    <main>
-      <Outlet />
-    </main>
-
-
-  </div>
+  return (
+    <div className ="root-layout">
+      <header>
+        page header here
+      </header>
+    
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
