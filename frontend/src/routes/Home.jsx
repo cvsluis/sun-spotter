4dc8493438c15156826b476a7bf25eb00b709a39
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-15 15:35:59 -0800

    Update class names and styling

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,25 +1,34 @@
+import '../styles/Home.scss';
+
 export default function Home () {
   return <div>
 
-      <div className="welcome-section">
-        <img alt="sunset" src={process.env.PUBLIC_URL + "/sunset.jpeg"}></img>
+      <header>
+      <img className='header__img' alt="sunset" src={process.env.PUBLIC_URL + "/sunset.jpg"}></img>
 
+      <div className="welcome__section">
         <h4>Welcome, user! It's 2 hours to sunset in Victoria</h4>
 
+        <div className="search__wrapper">
         <form action="/search" method="get">
           <input
+            className='search__bar'
             type="text"
             id="search"
             placeholder="Search by city"
           ></input>
-          <button type="submit">Search</button>
         </form>
+        </div>
       </div>
 
-      <div className='spots-near-user'>
+      </header>
+
+      <div className='spots__near-user'>
+        <h1>Local favourites near Victoria</h1>
       </div>
 
-      <div className='saved-spots'>
+      <div className='spots__saved'>
+        <h1>Your favourites sunset spots</h1>
       </div>
       
     </div>
