f942dd1ac9c781b6a15988727a61e3d1ec45e608
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-15 11:21:48 -0800

    feat: add map container

---
+++ frontend/src/routes/Spots.jsx
@@ -0,0 +1,9 @@
+export default function Spot() {
+  return (
+    <>
+    <h1>Google Map</h1>
+    <div className="map-container" style={{width: "800px", height: "600px", backgroundColor: 'lightgrey', margin: '2rem auto 0'}}>
+    </div>
+    </>
+  )
+}
