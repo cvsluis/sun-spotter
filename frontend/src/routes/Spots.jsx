b68e7017640160c158cf46dde54250cb77129350
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-15 12:25:55 -0800

    feat: add google map

--- frontend/src/routes/Spots.jsx
+++ frontend/src/routes/Spots.jsx
@@ -1,9 +1,41 @@
-export default function Spot() {
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 48.444689, // default latitude
+  lng: -123.331720, // default longitude
+};
+
+const Spot = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyCDGmiLZi277Uo_aQFMSG8fX04TiLvbZ3s',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
   return (
-    <>
-    <h1>Google Map</h1>
-    <div className="map-container" style={{width: "800px", height: "600px", backgroundColor: 'lightgrey', margin: '2rem auto 0'}}>
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={12.5}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
-    </>
-  )
-}
+  );
+};
+
+export default Spot;
