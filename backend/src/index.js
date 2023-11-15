2507ec24cb280a1b955a367a793f327c56595701
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-14 16:23:37 -0800

    feat: add spots api route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
+// load .env data into process.env
+require('dotenv').config();
+
+// Web server config
 const express = require('express');
+const port = process.env.PORT || 8080;
 const app = express();
 
-require('dotenv').config();
-const port = process.env.PGPORT || 3000;
+const db = require("./db");
 
+// Separated Routes for each Resource
+const spots = require("./routes/spots");
+
+// Mount all resource routes
+app.use("/api", spots(db));
+
+// Home page
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
