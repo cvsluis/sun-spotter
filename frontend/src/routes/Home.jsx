b6951c93fc20a69b354a8f580fb5f44a1ecb5dda
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-15 12:30:53 -0800

    Merge update

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,4 +1,31 @@
+import TopNavBar from "../components/TopNavBar";
 
-export default function Home () {
-  return <div>A homepage!</div>
+export default function Root() {
+  return (
+    <div>
+      <TopNavBar />
+
+      <div class="welcome-section">
+        <img alt="sunset" src={process.env.PUBLIC_URL + "/sunset.jpeg"}></img>
+
+        <h4>Welcome, user! It's 2 hours to sunset in Victoria</h4>
+
+        <form action="/search" method="get">
+          <input
+            type="text"
+            id="search"
+            placeholder="Search by city"
+          ></input>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      <div class='spots-near-user'>
+      </div>
+
+      <div class='saved-spots'>
+      </div>
+      
+    </div>
+  );
 }
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import TopNavBar from "../components/TopNavBar";
-
-export default function Root() {
-  return (
-    <div>
-      <TopNavBar />
-
-      <div class="welcome-section">
-        <img alt="sunset" src={process.env.PUBLIC_URL + "/sunset.jpeg"}></img>
-
-        <h4>Welcome, user! It's 2 hours to sunset in Victoria</h4>
-
-        <form action="/search" method="get">
-          <input
-            type="text"
-            id="search"
-            placeholder="Search by city"
-          ></input>
-          <button type="submit">Search</button>
-        </form>
-      </div>
-
-      <div class='spots-near-user'>
-      </div>
-
-      <div class='saved-spots'>
-      </div>
-      
-    </div>
-  );
-}
-=======
 import { Outlet } from "react-router-dom";
 
 export default function Root () {
@@ -46,4 +13,3 @@ export default function Root () {
     </div>
   );
 }
->>>>>>> ed0b7938333ab4e41391ab4f4ef0d26d84241e74
