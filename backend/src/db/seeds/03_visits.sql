8f106a69c6dd0e5699de3c45051bfa7a5fa4d7bc
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-14 16:56:21 -0800

    Add VISITS seeds

--- backend/src/db/seeds/03_visits.sql
+++ backend/src/db/seeds/03_visits.sql
@@ -4,8 +4,8 @@ VALUES
   (1, 5, 'Mossy Glory', 'public/images/mossrock.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
   (1, 10, 'Summer Eve', 'public/images/NEEDSPIC', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
   (2, 1, 'Great Views!', 'public/images/saxe.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
-  (2, 5, 'Moss Rocks!', 'public/images/NEEDSPIC', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
+  (2, 5, 'Moss Rocks!', 'public/images/moss_rock.jpg', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
   (2, 7, 'Ocean Sunset', 'public/images/harling_point.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
   (3, 3, 'Cloudy Skies', 'public/images/breakwater.jpg', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
-  (3, ),
-  ();
+  (3, 5, 'Amazing Victoria Views!', 'public/images/mossRock.jpg', 5, 'Best sunset in Vic!', '2023-03-9 16:30:00', DEFAULT),
+  (3, 2, 'Clover Sunset', 'public/images/clover_point.jpg', 4, 'Always beautiful, sometimes too windy!!', '2023-06-20 18:30:00', DEFAULT);
