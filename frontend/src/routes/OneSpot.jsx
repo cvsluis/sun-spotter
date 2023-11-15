d39cdbff9edf19d25dcd7a41d75f329b487da713
Janay Ma <janay.ma7@gmail.com>
2023-11-15 13:59:30 -0800

    add fetch to one spot from api

--- frontend/src/routes/OneSpot.jsx
+++ frontend/src/routes/OneSpot.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 export default function OneSpot () {
+
+  const [ spotInfo, setSpotInfo ] = useState();
+
+    
+  useEffect(() => {
+    fetch('http://localhost:8080/api/spot/1')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className='one-spot'>
       One spot view!!
