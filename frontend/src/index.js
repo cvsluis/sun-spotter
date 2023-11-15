d2936222397720333259d3c8d84d1d02d7ed42fb
Janay Ma <janay.ma7@gmail.com>
2023-11-15 11:18:03 -0800

    move scss files to styles folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
