3e2361f56d911ee609ad55f8cbad411f4af403d9
Janay Ma <janay.ma7@gmail.com>
2023-11-14 16:38:50 -0800

    refactor to create root outside of react tree

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//setup browser router, root to home page
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home Page! Welcome to sunspotter</div>
+  }, 
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
