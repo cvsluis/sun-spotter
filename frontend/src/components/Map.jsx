a00bf1be67875a78e8810db2420d2df2887ae51d
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-15 13:16:20 -0800

    feat: load spots data on map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 
 const libraries = ['places'];
@@ -15,6 +15,22 @@ const center = {
 };
 
 export default function Map() {
+  // spots for markers state
+  const [spots, setSpots] = useState([]);
+
+  // fetch data from backend, set it to spots state
+  useEffect(() => {
+    fetch('http://localhost:8080/api/spots')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setSpots(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyCDGmiLZi277Uo_aQFMSG8fX04TiLvbZ3s',
     libraries,
@@ -28,6 +44,13 @@ export default function Map() {
     return <div>Loading maps</div>;
   }
 
+  // for each spot, create marker component
+  const markerList = spots.map(spot => {
+    return (
+      <MarkerF key={spot.id} position={{lat: Number(spot.lat), lng: Number(spot.lng)}} />
+    );
+  });
+
   return (
     <div>
       <GoogleMap
@@ -35,7 +58,7 @@ export default function Map() {
         zoom={12.5}
         center={center}
       >
-        <MarkerF position={center} />
+        {markerList}
       </GoogleMap>
     </div>
   );
