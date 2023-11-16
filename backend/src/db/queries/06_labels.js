43bec7ea390abf62499c850594a91c5587a0c1e6
Janay Ma <janay.ma7@gmail.com>
2023-11-15 20:31:16 -0800

    move labels query into spot queries

--- backend/src/db/queries/06_labels.js
+++ backend/src/db/queries/06_labels.js
@@ -8,22 +8,6 @@ const getAllLabels = () => {
     });
 };
 
-// Get All Labels associated with one spot and their respective counts
-const getAllSpotLabels = (id) => {
-  const query = `SELECT labels.id as id, labels.name as name, COUNT(visits.name) 
-                  FROM spots JOIN visits 
-                  ON spots.id = visits.spot_id 
-                  JOIN visit_labels 
-                  ON visits.id = visit_labels.visit_id 
-                  JOIN labels 
-                  ON labels.id = visit_labels.label_id 
-                  WHERE spots.id = $1 
-                  GROUP BY labels.id, labels.name;`
 
-  return db.query(query, [id])
-  .then(data => {
-    return data.rows;
-  });
-};
 
 module.exports = { getAllLabels, getAllSpotLabels };
