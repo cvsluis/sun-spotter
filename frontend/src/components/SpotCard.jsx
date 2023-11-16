9354291b61c91f93a251aa9008e82723d577d676
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-16 13:37:56 -0800

    feat: display most recent visit image for spot

--- frontend/src/components/SpotCard.jsx
+++ frontend/src/components/SpotCard.jsx
@@ -6,7 +6,7 @@ export default function SpotCard({ spot }) {
   return (
     <div className='spotCard__container'>
       <div className='spotCard__image'>
-        <img src={image} />
+        <img src={`http://localhost:8080/${spot.image_url}`} />
       </div>
       <div className='spotCard__details'>
         <div className='spotCard__header'>
