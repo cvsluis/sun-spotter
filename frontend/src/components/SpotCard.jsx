41c4eb2b274b89af7bafd83fad22b03278e406fb
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-16 14:46:05 -0800

    feat: add label list and spot rating count ui

--- frontend/src/components/SpotCard.jsx
+++ frontend/src/components/SpotCard.jsx
@@ -12,10 +12,10 @@ export default function SpotCard({ spot }) {
       <div className='spotCard__details'>
         <div className='spotCard__header'>
           <h2>{spot.name}</h2>
-          <h4>* {spot.rating} <span>({spot.rating_count})</span></h4>
+          <h5>* {spot.rating} <span>({spot.rating_count})</span></h5>
         </div>
         <h3>{spot.city}, {spot.province}</h3>
-        <h3>{labelList}</h3>
+        <h4>{labelList}</h4>
       </div>
     </div>
   );
