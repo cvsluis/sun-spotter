8dbef819000ecbfd885bafbb59d6ae928e121c19
Janay Ma <janay.ma7@gmail.com>
2023-11-16 15:26:07 -0800

    make every second label grey

--- frontend/src/components/AboutSpot.jsx
+++ frontend/src/components/AboutSpot.jsx
@@ -3,7 +3,6 @@ import '../styles/AboutSpot.scss'
 
 export default function AboutSpot({spotData, spotLabels}) {
 
-  console.log("labels are: ", spotLabels);
 
   return (
     <div className='about-spot'>
@@ -17,9 +16,14 @@ export default function AboutSpot({spotData, spotLabels}) {
 
       {/* Spot labels */}
       <div className='about-spot__labels'>  
-        {spotLabels.length > 0 && spotLabels.map(label => (
-          <div className="about-spot__label">{label.name} ({label.count})</div>
-        ))}
+        {spotLabels.length > 0 && spotLabels.map((label, i) => {
+
+          // make every second label grey
+          if (i % 2 === 0) {
+            return <div key={label.id} className="about-spot__label">{label.name} ({label.count})</div>;
+          }
+          return <div key={label.id} className="about-spot__label about-spot__label--grey">{label.name} ({label.count})</div>;
+        })}
         {spotLabels.length === 0 && <div className='about-spot__labels'>no labels yet!</div>}
       </div>
     </div>
