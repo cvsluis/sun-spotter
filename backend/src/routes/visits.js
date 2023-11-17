43f359cb7927719f1614a015c421518f2f31c11c
Janay Ma <janay.ma7@gmail.com>
2023-11-16 20:22:28 -0800

    write /visits/:spotName api route

--- backend/src/routes/visits.js
+++ backend/src/routes/visits.js
@@ -4,10 +4,13 @@ const visitQueries = require('../db/queries/03_visits');
 module.exports = db => {
 
 // get visits by spot name
-router.get('/visits/:', (req, res) => {
-  labelQueries.getAllLabels()
-    .then(labels => {
-      res.json({ labels });
+router.get('/visits/:spotName', (req, res) => {
+
+  const spotName = req.params.spotName;
+
+  visitQueries.getVisitsBySpotName(spotName)
+    .then(visits => {
+      res.json({ visits });
     })
     .catch(err => {
       res
