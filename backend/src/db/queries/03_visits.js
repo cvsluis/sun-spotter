f0a8d501b27953996ca36d3d022d662317b56195
Janay Ma <janay.ma7@gmail.com>
2023-11-16 20:18:27 -0800

    add get visits by spot ID query

--- backend/src/db/queries/03_visits.js
+++ backend/src/db/queries/03_visits.js
@@ -1,3 +1,10 @@
 const db = require('../index');
 
+const getVisitsBySpotID = function (spotID) {
+  const query = `SELECT * FROM visits WHERE spot_id = $!`
+  db.query(query, [spotID])
+    .then(data => data.rows) ;
+}
+
+
 module.exports = {};
