1bdd3d61643f8175c6e83452363894789624a8fb
Janay Ma <janay.ma7@gmail.com>
2023-11-16 23:43:27 -0800

    refactor with conditional rendering, change color

--- frontend/src/components/AboutSpot.jsx
+++ frontend/src/components/AboutSpot.jsx
@@ -23,13 +23,14 @@ export default function AboutSpot({spotData, spotLabels, spotRating}) {
 
       {/* Spot labels */}
       <div className='about-spot__labels'>  
-        {spotLabels.length > 0 && spotLabels.map((label, i) => {
-          // make every second label grey
-          if (i % 2 === 0) {
-            return <div key={label.id} className="about-spot__label">{label.name} ({label.count})</div>;
-          }
-          return <div key={label.id} className="about-spot__label about-spot__label--grey">{label.name} ({label.count})</div>;
-        })}
+
+        {spotLabels.length > 0 && spotLabels.map((label, i) => (
+          //make every second label pink
+          i % 2 === 0 ? 
+          <div key={label.id} className="about-spot__label">{label.name} ({label.count})</div> :
+          <div key={label.id} className="about-spot__label about-spot__label--pink">{label.name} ({label.count})</div>
+        ))}
+
         {spotLabels.length === 0 && <div className='about-spot__labels'>no labels yet!</div>}
       </div>
     </div>
