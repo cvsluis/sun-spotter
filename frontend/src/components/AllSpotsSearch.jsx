0fada5477c8f678303a85b301754cb3b5433336b
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-16 20:26:53 -0800

    feat: add all spots search component

---
+++ frontend/src/components/AllSpotsSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/AllSpotsSearch.scss';
+
+
+export default function AllSpotsSearch() {
+  return (
+    <div className='allSpotsSearch__container'>
+
+    </div>
+  );
+};
