802ce8193832e8b6b83f86308db487d9babcee5f
Janay Ma <janay.ma7@gmail.com>
2023-11-17 00:05:33 -0800

    add visitCard stylesheet

--- frontend/src/components/VisitCard.jsx
+++ frontend/src/components/VisitCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+//import styles
+import '../styles/VisitCard.scss'
+
 export default function VisitCard({ visit }) {
 
   console.log(visit);
