cd21878d03638ec811af813c14936666bf1914cc
Janay Ma <janay.ma7@gmail.com>
2023-11-16 23:57:52 -0800

    add visits fetch to useSpotData

--- frontend/src/routes/OneSpot.jsx
+++ frontend/src/routes/OneSpot.jsx
@@ -16,14 +16,14 @@ import '../styles/OneSpot.scss';
 export default function OneSpot() {
 
   const spotID = useParams().id;
-  const [ spotData, spotLabels, spotRating ] = useSpotData(spotID);
+  const [ spotInfo, spotLabels, spotRating, spotVisits ] = useSpotData(spotID);
   
   return (
     <div className='one-spot'>
       <header className='one-spot__header'>
-        <OneSpotMap lng={spotData.lng} lat={spotData.lat} />
+        <OneSpotMap lng={spotInfo.lng} lat={spotInfo.lat} />
         <div>
-          <AboutSpot spotData={spotData} spotLabels={spotLabels} spotRating={spotRating}/> 
+          <AboutSpot spotData={spotInfo} spotLabels={spotLabels} spotRating={spotRating}/> 
           <Link className={'one-spot__create-visit'} to="/">Add Visit</Link>
         </div>
       </header>
