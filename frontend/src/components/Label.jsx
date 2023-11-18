331458ba64743dc6e57eb97265597846869e2610
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-18 10:51:37 -0800

    feat: add default props to label component

--- frontend/src/components/Label.jsx
+++ frontend/src/components/Label.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Label.scss';
 
-export default function Label({ name }) {
+export default function Label({ name, active }) {
   return (
-    <div className='label label__inactive'>
+    <div className={`label label__inactive ${active && 'label__active'}`}>
       {name}
     </div>
   );
+};
+
+Label.defaultProps = {
+  name: 'Label Name Goes Here',
+  active: false
 };
