d06c4fc81b1d2263b49e2724731d7293b6f4eb36
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-17 17:03:18 -0800

    feat: add container for form and map

--- frontend/src/routes/CreateSpot.jsx
+++ frontend/src/routes/CreateSpot.jsx
@@ -1,6 +1,19 @@
+import '../styles/CreateSpot.scss';
+import Map from '../components/Map';
+
 export default function CreateSpot() {
   return (
-    <div>
+    <div className='one-spot createSpot__container'>
+
+      <div className='createSpot__sideBar'>
+        <h1>Create a Spot</h1>
+        <form>
+        </form>
+      </div>
+
+      <div className='createSpot__map'>
+        <Map spots={[]} handlePinClick={null} borderRadius={true}/>
+      </div>
 
     </div>
   );
