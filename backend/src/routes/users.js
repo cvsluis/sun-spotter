26d48ceca3849fe0e79b9377f8c1fa983e8df100
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-18 15:16:15 -0800

    Create db user route

---
+++ backend/src/routes/users.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const userQueries = require('../db/queries/01_users');
+
+module.exports = db => {
+  
+  router.get('/users', (req, res) => {
+    userQueries.getUsers()
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ erros:err.message })
+    });
+  });
+  return router;
+};
