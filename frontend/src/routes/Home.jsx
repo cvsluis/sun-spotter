f98c53a6e2a3fa9837300080de7c06e2519b01e2
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-18 10:07:43 -0800

    Format

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,43 +1,46 @@
-import '../styles/Home.scss';
-import sunset from '../assets/sunset.jpeg'
-import SpotCard from '../components/SpotCard';
+import "../styles/Home.scss";
+import sunset from "../assets/sunset.jpeg";
+import SpotCard from "../components/SpotCard";
 
-export default function Home () {
-
-  return <div>
-
+export default function Home() {
+  return (
+    <div>
       <header>
-      <img className='header__img' alt="sunset" src={sunset}></img>
+        <img className="header__img" alt="sunset" src={sunset}></img>
 
-      <div className="welcome__section">
-        <h4>Welcome, user! It's 2 hours to sunset in Victoria</h4>
+        <div className="welcome__section">
+          <h4>Welcome, user! It's 2 hours to sunset in Victoria</h4>
 
-        <div className="search__wrapper">
-        <form action="/search" method="get">
-          <input
-            className='search__bar'
-            type="text"
-            id="search"
-            placeholder="Search by city"
-          ></input>
-        </form>
+          <div className="search__wrapper">
+            <form action="/search" method="get">
+              <input
+                className="search__bar"
+                type="text"
+                id="search"
+                placeholder="Search by city"
+              ></input>
+            </form>
+          </div>
         </div>
-      </div>
-
       </header>
 
-      <div className='spots__near-user'>
+      <div className="spots__near-user">
         <h1>Local favourites near Victoria</h1>
-        <SpotCard />
-        <SpotCard />
-        <SpotCard />
-
+        <div className="spots__carousel"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
 
-      <div className='spots__saved'>
+      <div className="spots__saved">
         <h1>Your favourites sunset spots</h1>
+        <div className="spots__carousel"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-      
     </div>
-    
+  );
 }
