af715a98bac3a17ee62a2b9c32ecabbaea11b019
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-18 15:01:58 -0800

    Style form.

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -4,26 +4,33 @@ import '../styles/Login.scss';
 export default function Login () {
   return (
     <div className="login__container">
-      <h2>Login</h2>
-      <form>
+      <div className="login__welcome">
+      <h2>Welcome back.</h2>
+      <h2>Log in and chase the sun.</h2>
+      </div>
+      <form className="login__form">
         <div>
-          <label>Username:</label>
           <input 
+            className="login__input"
             type="text" 
-            id='username'            
+            id='username'
+            placeholder='Username'       
             />
         </div>
         <div>
-          <label>Password:</label>
           <input 
+            className="login__input"
             type="text" 
             id='password'
+            placeholder='Password'
           />
         </div>
-        <button type='submit'>
-            Login
+        <button type='submit' className="login__btn">
+            Log in
         </button>
       </form>
+      <p>Forgot your password?</p>
+      <p>Don't have an account?<strong>Sign up for free</strong></p>
     </div>
 
 
