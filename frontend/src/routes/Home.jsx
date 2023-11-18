d60b345086524c8297b60e4ae4d7222b29932c01
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-18 11:35:16 -0800

    Add basic carousel component

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,8 +1,33 @@
+import React, { useState } from 'react';
+import SpotCarousel from '../components/SpotCarousel';
 import "../styles/Home.scss";
 import sunset from "../assets/sunset_header.jpg";
-import SpotCard from "../components/SpotCard";
+// import SpotCard from "../components/SpotCard";
 
 export default function Home() {
+
+  // all spots state
+  const [spots, setSpots] = useState([]);
+
+  const fetchAllSpots = (searchInput) => {
+    let url = 'http://localhost:8080/api/spots';
+
+    if (searchInput) {
+      url += `?search=${searchInput.trim()}`;
+    }
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setSpots(data))
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
+
   return (
     <div>
       <header>
@@ -30,19 +55,13 @@ export default function Home() {
       <div className="spots__near-user">
         <h1>Local favourites near Victoria</h1>
         <div className="spots__carousel"></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <SpotCarousel />
       </div>
 
       <div className="spots__saved">
         <h1>Your favourites sunset spots</h1>
         <div className="spots__carousel"></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <SpotCarousel />   
       </div>
     </section>
 
