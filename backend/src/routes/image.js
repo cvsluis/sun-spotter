f60b6e2b64ef6713630cd08ebd74790b52137706
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-19 14:02:13 -0800

    feat: set up multer for image upload

---
+++ backend/src/routes/image.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const multer = require("./multer");
+
+module.exports = db => {
+
+  // Get All Labels
+  router.get('/image', (req, res) => {
+    res.json({ 'hi' : 'hi'});
+  });
+
+  router.post("/image", multer.uploadImg.single("file"), async (req, res, next) => {
+    const file = req.file;
+
+    if (!file) {
+      const error = new Error('Please upload a file');
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+
+    res.status(200).send("File saved!");
+  });
+
+  return router;
+};
