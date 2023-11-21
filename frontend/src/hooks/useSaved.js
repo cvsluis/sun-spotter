e4a6ead365565ee6130829351e5a2b8e20f201d5
Janay Ma <janay.ma7@gmail.com>
2023-11-20 23:14:20 -0800

    add comment

--- frontend/src/hooks/useSaved.js
+++ frontend/src/hooks/useSaved.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 export default function useSaved() {
   const[ isSaved, setIsSaved ] = useState(false);
   
+
+  //will need to handle posts to saves once user auth is implemented
   const toggleSaved = function() {
     setIsSaved(!isSaved);
   }
