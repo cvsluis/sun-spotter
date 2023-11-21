77c7dc47a5678e8021e23bf48180deb54d598d0c
Janay Ma <janay.ma7@gmail.com>
2023-11-20 17:24:12 -0800

    create conditions card and sunset time card

--- frontend/src/components/WeatherBar.jsx
+++ frontend/src/components/WeatherBar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+//import components
+import SunsetTimeCard from './SunsetTimeCard';
+import ConditionsCard from './ConditionsCard';
+
+
 export default function WeatherBar({ weather }) {
+
+
   return (
   <>
     <div className="weather-bar__sunset-time">
       <h3>Sunset Time</h3>
-
+        <SunsetTimeCard />
     </div>
 
     <div className="weather-bar__conditions">
       <h3>Conditions</h3>
+        <ConditionsCard />
     </div>
   </>
   )
