2bf394bec7f1cf09c2c6c0048d76b18d4bab0b70
Janay Ma <janay.ma7@gmail.com>
2023-11-21 15:19:48 -0800

    import icons into conditions card

--- frontend/src/components/ConditionsCard.jsx
+++ frontend/src/components/ConditionsCard.jsx
@@ -5,10 +5,17 @@ import parseWeatherCode from "../utils/parseWeatherCode";
 export default function ConditionsCard({ conditions }) {
   
   const weatherType = parseWeatherCode(conditions.weather_code);
+  const iconPath = `../assets/weather-icons/${weatherType}.png`;
+  console.log(iconPath)
+ 
   return(
     <div className="conditions-card">
       <div className="conditions-card__day">{conditions.day}</div>
-      <div className="conditions-card__weather">code: {weatherType}</div>
+      <div className="conditions-card__weather">
+        <img src={ require(iconPath) }></img>
+
+      
+      </div>
       <div className="conditions-card__max-temp">max: {conditions.max_temp}</div>
       <div className="conditions-card__min-temp">min: {conditions.min_temp }</div>
     </div>
