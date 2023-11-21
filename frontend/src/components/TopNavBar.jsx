f1ccc85732931aeb36e210e70c9e0d2f244ec76e
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-21 10:45:02 -0800

    Add explore link to spots view

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import '../styles/TopNavBar.scss';
 import logo from '../assets/logo.png'
 
 export default function TopNavBar() {
   return (
     <nav>
+      <div>
       <img className='nav__logo' alt='logo' src={logo}></img>
+      <Link to={`/spots`} className='nav__explore-link'>Explore</Link>
+      </div>
 
       <div>
         <button className='nav__login-btn'>Log In</button>
