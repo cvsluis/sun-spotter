e46adf9600c01604ee5d7a362f4a5c29b63a4393
Janay Ma <janay.ma7@gmail.com>
2023-11-20 21:08:04 -0800

    log weather conditions on onespot page

--- frontend/src/components/ConditionsCard.jsx
+++ frontend/src/components/ConditionsCard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function ConditionsCard({ conditions }) {
-  //console.log(conditions)
+  console.log(conditions)
   return(
-    <div>card</div>
+    <div className="conditions-card">
+      <div className="conditions-card__day">{conditions.day}</div>
+      <div className="conditions-card__weather">code: {conditions.weather_code}</div>
+      <div className="conditions-card__max-temp">max: {conditions.max_temp}</div>
+      <div className="conditions-card__min-temp">min: {conditions.min_temp }</div>
+    </div>
   )
 }
