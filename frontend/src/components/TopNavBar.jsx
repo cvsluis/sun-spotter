6e82578987c3091f835f4a81196b478de5d04171
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-21 11:08:51 -0800

    Fix duplicate link bug

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import '../styles/TopNavBar.scss';
-import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 export default function TopNavBar() {
