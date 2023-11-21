1c79be1f2b32d7a1149211dfb4d49cb6c7c02230
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-21 11:16:33 -0800

    Refactor

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,65 +1,21 @@
-import React, { useState, useEffect } from "react";
-import  { Link } from 'react-router-dom'
-
-
 //import styles
 import "../styles/Login.scss";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [users, setUsers] = useState([]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const fetchUsers = () => {
-    let url = "http://localhost:8080/api/users";
-
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => setUsers(data))
-      .catch((error) => console.error("Error fetching data:", error));
-
-    };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const handleLogin = (event) =>{
-    event.preventDefault();
-
-    const user = users.find((user) => user.email === email);
-
-    if (user && user.password === password) {
-      setIsLoggedIn(true);
-      return <Link to='/home' />
-    } else {
-      setIsLoggedIn(false);
-    }
-  };
-
+  
   return (
     <div className="login__container">
       <div className="login__welcome">
         <h2>Welcome back.</h2>
         <h2>Log in and chase the sun.</h2>
       </div>
-      <form 
-        className="login__form"
-        onSubmit={handleLogin}>
+      <form className="login__form">
         <div>
           <input
             className="login__input"
             type="text"
             id="email"
             placeholder="Email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
@@ -68,8 +24,6 @@ export default function Login() {
             type="text"
             id="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <button type="submit" className="login__btn">
