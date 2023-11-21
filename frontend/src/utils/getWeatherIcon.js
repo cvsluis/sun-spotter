3672c40e084182e01207e62002d6504823d21bce
Janay Ma <janay.ma7@gmail.com>
2023-11-21 15:40:38 -0800

    u

--- frontend/src/utils/getWeatherIcon.js
+++
@@ -1,7 +0,0 @@
-function getWeatherIcon (weatherType) {
-  let iconPath = "";
-  switch (weatherType) {
-    case "sun":
-
-  }
-}
--- frontend/src/utils/parseWeatherCode.js
+++ frontend/src/utils/parseWeatherCode.js
@@ -24,16 +24,15 @@ export default function parseWeatherCode(code) {
   const rainCodes = Array.from([...Array(20).keys()], n => n + 50);
   const snowCodes = Array.from([...Array(10).keys()], n => n + 70);
  
-  //still need to assign: 02, 19
   const codes = {
-    cloudsAndSun: [1, 3],
+    cloudsAndSun: [1, 3, 2],
     clouds: fogCodes,
     sun: [0],
     sunAndRain: [14, 15, 16, 17, 18, 20, 21, 24, 25],
     rain: rainCodes.concat([80, 81, 82, 91, 92]),
     snow: [36, 37, 38, 39, 83, 84, 85, 86, 87, 88, 89, 90, 93, 94].concat(snowCodes),
     sunAndSnow: [22, 23, 26, 27],
-    thunder: [13, 17, 29, 95, 96, 97, 98, 99],
+    thunder: [13, 17, 19, 29, 95, 96, 97, 98, 99],
     hazy: [4, 5, 6, 7, 8, 9, 10, 11, 12, 28,30, 31, 32, 33, 34, 35]
   }
 
@@ -43,5 +42,3 @@ export default function parseWeatherCode(code) {
     }
   }
 } 
-
-console.log(parseWeatherCode(20));
