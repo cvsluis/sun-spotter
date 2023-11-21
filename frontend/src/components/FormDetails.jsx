fd8489709676f9f4dbbdaec3a32dc80556ba77e7
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-20 20:38:51 -0800

    fix: update 'for' tag and add values to inputs

--- frontend/src/components/FormDetails.jsx
+++ frontend/src/components/FormDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Label from './Label';
-import '../styles/FormDetails.scss'
+import '../styles/FormDetails.scss';
 
-export default function FormDetails({ handleFormChange, handleLabelClick, isClicked }) {
-    // list of labels from db
+export default function FormDetails({ handleFormChange, handleLabelClick, isClicked, visitValues }) {
+  // list of labels from db
   const [labels, setLabels] = useState([]);
 
   useEffect(() => {
@@ -30,23 +30,23 @@ export default function FormDetails({ handleFormChange, handleLabelClick, isClic
         <div className='formDetails__container--inputs'>
 
           <div className='formDetails__container--input'>
-            <label for="chosenName">What is this location called?</label>
-            <input onChange={handleFormChange} className='formDetails__input--text' placeholder='Add Name' id='createSpot__form-id--name' name='chosenName' autoComplete='off'></input>
+            <label htmlFor="chosenName">What is this location called?</label>
+            <input onChange={handleFormChange} value={visitValues.chosenName} className='formDetails__input--text' placeholder='Add Name' id='createSpot__form-id--name' name='chosenName' autoComplete='off'></input>
           </div>
 
           <div className='formDetails__container--input'>
-            <label for="time_stamp">When did you visit this sunset spot?</label>
-            <input onChange={handleFormChange} className='formDetails__input--text' type="datetime-local" id='createSpot__form-id--date-time' name='time_stamp'></input>
+            <label htmlFor="time_stamp">When did you visit this sunset spot?</label>
+            <input onChange={handleFormChange} value={visitValues.time_stamp} className='formDetails__input--text' type="datetime-local" id='createSpot__form-id--date-time' name='time_stamp'></input>
           </div>
 
           <div className='formDetails__container--input'>
-            <label for="description">Now tell us about this spot! How was the sunset?</label>
-            <textarea onChange={handleFormChange} className='formDetails__input--text' type="text" rows='8' maxLength="250" placeholder='Description' autoComplete='off' id='createSpot__form-id--description' name='description'></textarea>
+            <label htmlFor="description">Now tell us about this spot! How was the sunset?</label>
+            <textarea onChange={handleFormChange} value={visitValues.description} className='formDetails__input--text' type="text" rows='8' maxLength="250" placeholder='Description' autoComplete='off' id='createSpot__form-id--description' name='description'></textarea>
           </div>
 
         </div>
         <div className='formDetails__container--labels formDetails__container--input'>
-          <label for='labels'>Add some labels to let others know what this spot is about!</label>
+          <label htmlFor='labels'>Add some labels to let others know what this spot is about!</label>
           <div className='formDetails__labels--container' name='labels'>
             {labelList}
           </div>
@@ -54,7 +54,7 @@ export default function FormDetails({ handleFormChange, handleLabelClick, isClic
       </div>
 
       <div className='formDetails__container-rating'>
-        <label for='rating'>How would you rate this spot?</label>
+        <label htmlFor='rating'>How would you rate this spot?</label>
       </div>
 
     </div>
