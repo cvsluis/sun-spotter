654b70531ea4497f1237d84f54c112cde5595e1f
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-22 11:17:13 -0800

    feat: setup container styles

--- frontend/src/routes/OneVisit.jsx
+++ frontend/src/routes/OneVisit.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import '../styles/OneVisit.scss';
 
-
 export default function OneVisit() {
 
   const visitId = useParams().id;
 
   return (
-    <div></div>
+    <div className='one-visit__container'>
+      <div className='one-visit__details--container'>
+        {/* visit details */}
+      </div>
+      
+      <div className='one-visit__image--container'>
+        {/* image */}
+      </div>
+
+    </div>
   );
 }
