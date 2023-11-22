7e51efe703f2d8b0668d9fe442b5538f1dcecfdb
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-21 17:21:02 -0800

    Refactor

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -6,7 +6,7 @@ module.exports = db => {
   router.get('/users', (req, res) => {
     userQueries.getUsers()
     .then(users => {
-      res.json({ users });
+      res.json(users);
     })
     .catch(err => {
       res
@@ -15,20 +15,6 @@ module.exports = db => {
     });
   });
 
-  // Login
-  router.post('/login', (req, res) => {
-    const query = `SELECT * FROM users WHERE email=? AND password=?`
-    
-    db.query(query, [req.body.email, req.body.password], (err, data) => {
-      if(err) {
-        return res.json({Message: 'Server side error'})
-      }
-      
-      console.log(res.json(data));
-
-      return res.json({Status: 'Success'})
-    })
-  });
   
   return router;
 };
