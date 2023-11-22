378416549cbb8ca19dde5c41c0022adee2237273
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-22 11:32:36 -0800

    feat: fetch visit data

--- frontend/src/hooks/useVisitData.js
+++ frontend/src/hooks/useVisitData.js
@@ -1,5 +1,16 @@
 import { useState, useEffect } from 'react';
 
 export default function useVisitData(visitId) {
-  return [];
+  const [visitData, setVisitData] = useState({});
+
+  useEffect(() => {
+    const fetchVisit = async () => {
+      const visit = await fetch(`http://localhost:8080/api/visits/${visitId}`);
+      setVisitData(visit);
+    }
+
+    fetchVisit();
+  }, []);
+
+  return [visitData];
 }
