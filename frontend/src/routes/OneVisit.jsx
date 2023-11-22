0d1bfd037d110a142a84f803cab79374b3761c1e
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-22 14:13:11 -0800

    feat: style visit header

--- frontend/src/routes/OneVisit.jsx
+++ frontend/src/routes/OneVisit.jsx
@@ -11,7 +11,32 @@ export default function OneVisit() {
   return (
     <div className='one-visit__container'>
       <div className='one-visit__details--container'>
-        {visit.user_id}
+        <div className='one-visit__header--container'>
+
+          <div className='one-visit__header--profile'>
+            <div className='one-visit__header--image'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" fill="#757575" class="bi " viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+              </svg>
+            </div>
+
+            <div>
+              <h1>{visit.first_name} {visit.last_name}</h1>
+              {/* <p>{visit.time_stamp}</p> */}
+              <p>2 weeks ago</p>
+            </div>
+          </div>
+
+          <h2>{visit.rating}</h2>
+          <h3>{visit.description}</h3>
+
+        </div>
+
+        <div className='one-visit__comments--container'>
+
+        </div>
+
       </div>
       
       <div className='one-visit__image--container'>
