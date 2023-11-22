e880b37dffbab739aff9f53d88b8b748df32d9a0
Janay Ma <janay.ma7@gmail.com>
2023-11-21 16:53:34 -0800

    styling sunset card

--- frontend/src/components/SunsetTimeCard.jsx
+++ frontend/src/components/SunsetTimeCard.jsx
@@ -4,7 +4,10 @@ export default function SunsetTimeCard({ hour, minute }) {
   //console.log('the sunset time is ', hour, minute)
   return (
     <div className="sunset-time-card">
-      <img src={ require(`../assets/weather-icons/sunset.png`) } className={"weather-bar__sunset"} alt='sunset icon'></img> {hour % 12}:{minute}
+      <img src={ require(`../assets/weather-icons/sunset.png`) } className={"weather-bar__sunset"} alt='sunset icon'></img> 
+      <span>
+        {hour % 12}:{minute}
+      </span>
     </div>
   )
 }
