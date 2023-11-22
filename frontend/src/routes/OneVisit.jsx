9427e91e6583d836a91379fc5978d84e181366f5
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-22 11:33:59 -0800

    feat: use hook to get data

--- frontend/src/routes/OneVisit.jsx
+++ frontend/src/routes/OneVisit.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import '../styles/OneVisit.scss';
+import useVisitData from '../hooks/useVisitData';
 
 export default function OneVisit() {
 
   const visitId = useParams().id;
+  const [visitData] = useVisitData(visitId);
 
   return (
     <div className='one-visit__container'>
