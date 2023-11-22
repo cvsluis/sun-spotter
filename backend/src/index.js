1af65e5fda781a49bb9565e1799025a441bcbc7d
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-22 12:35:13 -0800

    Set cookie session for whole app

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,22 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 const cors = require('cors');
+const cookieSession = require("cookie-session");
 
 //Setup cross origin resource sharing
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['session-key'], 
+  // maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  // credentials: true
+})); 
+
+app.use(express.json());
 
 const db = require("./db");
 
@@ -26,7 +36,7 @@ const visits = require("./routes/visits");
 app.use("/api", spots(db));
 app.use("/api", labels(db));
 app.use("/api", users(db));
-app.use('/api', login);
+app.use('/api', login(db));
 app.use("/api", visits(db));
 
 // Home page
