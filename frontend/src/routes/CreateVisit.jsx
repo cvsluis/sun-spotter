f992a554e8d380e2c8b7008206307b2762b07ef8
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-21 19:22:56 -0800

    feat: add starting view for create visit

--- frontend/src/routes/CreateVisit.jsx
+++ frontend/src/routes/CreateVisit.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import BackButton from '../components/BackButton';
+import ForwardButton from '../components/ForwardButton';
+
+import '../styles/CreateSpot.scss';
 
 export default function CreateVisit() {
+  const spotID = useParams().id;
+
+  // fetch: post to /api/visits
+
   return (
-    <div>
-      
+    <div className='createSpot__container'>
+      <div className='createSpot__container--details'>
+        <h1>Add a Visit</h1>
+      </div>
+
+      <div>
+        <hr className='createSpot__line'></hr>
+
+        <div className='createSpot__container--nav'>
+          <BackButton handleBackClick={null} />
+          <ForwardButton handleForwardClick={null} modal={null} buttonOn={null} />
+        </div>
+      </div>
     </div>
   )
 }
