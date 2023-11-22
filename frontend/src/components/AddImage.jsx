4659b0b23b65f851ee4b5235081ad3f461a89cd8
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-21 16:46:10 -0800

    feat: add drop to upload title

--- frontend/src/components/AddImage.jsx
+++ frontend/src/components/AddImage.jsx
@@ -11,7 +11,7 @@ export default function AddImage({ handleFileInput, imagePreview }) {
               <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
               <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z" />
             </svg>
-            <h3>Click box to upload</h3>
+            <h3>Drag a file here or click to upload</h3>
             <p>Maximun file size 2MB</p>
             <input type="file" onChange={handleFileInput} name='image' id='createSpot__form-id--image' />
           </div>
