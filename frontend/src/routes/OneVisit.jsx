064178a12fcee9576ddfdf9825851ab4b804b845
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-22 11:03:30 -0800

    feat: add setup for one visit view

---
+++ frontend/src/routes/OneVisit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import '../styles/OneVisit.scss';
+
+
+export default function OneVisit() {
+
+  const visitId = useParams().id;
+
+  return (
+    <div></div>
+  );
+}
