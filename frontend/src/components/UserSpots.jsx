84a86c5386f0ded90717370bd2b7563e9b1a90b5
Janay Ma <janay.ma7@gmail.com>
2023-11-22 11:56:58 -0800

    add conditional rendering for spots view

--- frontend/src/components/UserSpots.jsx
+++ frontend/src/components/UserSpots.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 import '../styles/UserSpots.scss';
 
-
 export default function UserSpots() {
+
+  //which spots view is being displayed?
+  const isMapView = false;
+  const isSaves = false;
+  const isVisits = true;
+
   return (
     <div className="user-spots__container">
-
+      <header className="user-spots__header">
+        <span className="user-spots__category">
+          { isMapView && <h2>Saves and Favourites</h2> }
+          { isSaves && <h2>My Saved Spots</h2> }
+          { isVisits && <h2>My Visited Spots</h2>}
+        </span>
+      </header>
     </div>
   );
 };
