888820a43a99303915de14312ea5ca2bc22a100a
Janay Ma <janay.ma7@gmail.com>
2023-11-22 18:12:15 -0800

    implement login + logout buttons

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,8 +1,18 @@
 import { Link } from "react-router-dom";
 import '../styles/TopNavBar.scss';
 import logo from '../assets/logo.png';
+import Cookies from 'js-cookie'
 
 export default function TopNavBar() {
+
+  let userID = Cookies.get('user_id')
+
+  const handleLogout = function (e, userID) {
+    e.preventDefault();
+    console.log('logging out!')
+    Cookies.remove('user_id');
+    userID = 0;
+  }
   return (
     <nav>
       <div className='nav__main'>
@@ -10,10 +20,18 @@ export default function TopNavBar() {
       <Link to={`/spots`} className='nav__explore-link'>Explore</Link>
       </div>
 
+      {!userID && 
       <div>
         <Link to='/login' className='nav__login-btn'>Log In</Link>
         <button className='nav__register-btn'>Register</button>
       </div>
+      }
+
+      {userID && 
+      <div>
+        <button className="'nav__logout-btn" onClick={handleLogout}>Log Out</button>
+      </div>
+      }
     </nav>
   )
 }
