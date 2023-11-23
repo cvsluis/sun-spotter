af2925d48e8adc12498427165185e649301d4568
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-23 15:27:20 -0800

    Refactor

--- backend/src/db/queries/04_saves.js
+++ backend/src/db/queries/04_saves.js
@@ -1,11 +1,3 @@
 const db = require('../index');
 
-
-//get user saved spots
-const userSpots = function (userID) {
-  const query = `SELECT spot_id FROM saves WHERE user_id=$1;`;
-  return db.query(query, [userID])
-  .then(data => data.rows);
-}
-
-module.exports = {userSpots};
+module.exports = {};
