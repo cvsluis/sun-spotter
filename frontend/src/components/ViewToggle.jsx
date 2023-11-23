1f440417e0b3f2348e99b83789c7775ebfd1d62d
Janay Ma <janay.ma7@gmail.com>
2023-11-22 23:40:00 -0800

    conditinal toggle bar depending on view state

--- frontend/src/components/ViewToggle.jsx
+++ frontend/src/components/ViewToggle.jsx
@@ -2,12 +2,40 @@ import React from 'react';
 
 import '../styles/ViewToggle.scss';
 
-export default function() {
+export default function({ view, setView}) {
+  
+  const handleToggleClick = function(event, viewName ) {
+    event.preventDefault();
+
+    console.log(`${viewName} clicked!`)
+
+    if (view !== viewName) {
+      setView(viewName);
+    }
+  }
+
+  console.log('view is: ', view)
+
   return (
     <div className='view-toggle'>
-      <div className='view-toggle__option'>Map</div>
-      <div className='view-toggle__option'>Saved</div>
+
+      {view === 'map' ?
+      <div className='view-toggle__option view-toggle__option--selected'>Map</div>
+      :
+      <div className='view-toggle__option' onClick={(event) => handleToggleClick(event, 'map')}>Map</div>
+      }
+
+      {view === 'saved' ?
+      <div className='view-toggle__option view-toggle__option--selected'>Saved</div>
+      :
+      <div className='view-toggle__option' onClick={(event) => handleToggleClick(event, 'saves')}>Saved</div>
+      }
+
+      {view === 'visits' ?
       <div className='view-toggle__option view-toggle__option--selected'>Visits</div>
+      :
+      <div className='view-toggle__option' onClick={(event) => handleToggleClick(event, 'visits')}>Visits</div>
+      }
     </div>
 
   )
