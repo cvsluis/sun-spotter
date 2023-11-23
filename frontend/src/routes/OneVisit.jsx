e397dfefb99d514feb7a9a51de79a0d9b6413833
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 14:32:55 -0800

    feat: display user profile image

--- frontend/src/routes/OneVisit.jsx
+++ frontend/src/routes/OneVisit.jsx
@@ -20,12 +20,7 @@ export default function OneVisit() {
         <div className='one-visit__header--container'>
 
           <div className='one-visit__header--profile'>
-            <div className='one-visit__header--image'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" fill="#757575" className="bi " viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-              </svg>
-            </div>
+            <img src={`http://localhost:8080/${visit.profile_pic}`} className='one-visit__header--image'/>
 
             <div>
               <h2>{visit.first_name} {visit.last_name}</h2>
