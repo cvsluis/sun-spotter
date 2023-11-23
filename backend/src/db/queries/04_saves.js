8471605a4a4f44ee7501b3336084b4098b04b0e8
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-23 14:47:00 -0800

    Add query

--- backend/src/db/queries/04_saves.js
+++ backend/src/db/queries/04_saves.js
@@ -1,3 +1,11 @@
 const db = require('../index');
 
-module.exports = {};
+
+//get user saved spots
+const userSpots = function (userID) {
+  const query = `SELECT * FROM saves WHERE user_id=$1;`;
+  return db.query(query, [userID])
+  .then(data => data.rows);
+}
+
+module.exports = {userSpots};
