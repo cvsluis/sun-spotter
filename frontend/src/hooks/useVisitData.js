156bb71b9b6af134b3cc72c3c5ac0cfad8a49236
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 15:37:44 -0800

    feat: fetch visit labels data

--- frontend/src/hooks/useVisitData.js
+++ frontend/src/hooks/useVisitData.js
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react';
 
 export default function useVisitData(visitId) {
   const [visit, setVisit] = useState({});
+  const [labels, setLabels] = useState({});
 
   useEffect(() => {
     const fetchVisit = async () => {
       try {
         const visit = await fetch(`http://localhost:8080/api/visits/${visitId}`).then(res => res.json());
+        const labelList = await fetch(`http://localhost:8080/api/visits/${visitId}/labels`).then(res => res.json());
+
         setVisit(visit);
+        setLabels(labelList);
       } catch (err) {
         console.error('Error fetching data', err);
       }
@@ -16,5 +20,5 @@ export default function useVisitData(visitId) {
     fetchVisit();
   }, []);
 
-  return [visit];
+  return [visit, labels];
 }
