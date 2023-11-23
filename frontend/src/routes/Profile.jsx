e2e908d31e81e7244b4eb6926806b57ea6e37d45
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-23 09:36:23 -0800

    Initial fetch requests

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 
 //import componenets
 import UserInfoCard from "../components/UserInfoCard";
@@ -9,13 +10,32 @@ import '../styles/Profile.scss'
 
 export default function Profile() {
   const [user, setUser] = useState({});
+  const [userID, setUserID] = useOutletContext();
+
+  const findLoggedInUser = () => {
+
+  }
 
   const fetchUser = () => {
-    fetch(`http://localhost:8080/api/users/${user.id}`)
+    fetch(`http://localhost:8080/api/users/${user}`)
     .then((response) => {
+      if(!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => setUser(data))
+    .catch((error) => console.error('Error fetching data', error))
+  };
 
-    }) 
+  const fetchSavedUserSpots = () => {
+
   }
+
+  const fetchUserVisits = () => {
+
+  }
+
   return (
     <div className="profile">
       <UserInfoCard />
