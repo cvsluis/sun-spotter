6af42e45646bfff7d02d99626733ad78d4f4b720
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 14:32:22 -0800

    fix: rename files to be consistent, update visit query

--- backend/src/db/queries/03_visits.js
+++ backend/src/db/queries/03_visits.js
@@ -2,7 +2,7 @@ const db = require('../index');
 
 // Get One Visit
 const getOneVisit = (id) => {
-  return db.query(`SELECT visits.*, users.first_name, users.last_name FROM VISITS JOIN users ON visits.user_id = users.id WHERE visits.id = $1;`, [id])
+  return db.query(`SELECT visits.*, users.first_name, users.last_name, users.profile_pic FROM VISITS JOIN users ON visits.user_id = users.id WHERE visits.id = $1;`, [id])
     .then(data => {
       return data.rows[0];
     });
