1f9e3afe64c500701061f847c38b0a2831046f8a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-22 20:39:11 -0800

    Add hard coded profile info

--- frontend/src/components/UserInfoCard.jsx
+++ frontend/src/components/UserInfoCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import profile from "../assets/profile/janay.jpeg";
 
 import "../styles/UserInfoCard.scss"
 
 export default function UserInfoCard() {
   return (
     <div className="user-info__container">
-
+      <img src={profile} alt="profile" className="card__image" />
+      <h2 className="card__name">Janay Ma</h2>
+      <p className="card__location">Victoria, British Columbia</p>
     </div>
-  );
+ );
 };
+
