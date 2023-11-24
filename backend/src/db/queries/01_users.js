c0febe5a1c3a0633016f59bd55e15639415fdaaf
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-24 15:35:30 -0800

    feat: add lat lng to visit query

--- backend/src/db/queries/01_users.js
+++ backend/src/db/queries/01_users.js
@@ -15,8 +15,8 @@ const getUserByID = (id) => {
 };
 
 const getVisitsByUser = (userID) => {
-  query = `SELECT visits.id as id, visits.time_stamp as date, visits.image_url as image_url, visits.name as name
-   FROM visits WHERE visits.user_id = $1`
+  query = `SELECT visits.id as id, visits.time_stamp as date, visits.image_url as image_url, visits.name as name, spots.lat, spots.lng 
+   FROM visits JOIN spots ON spots.id = visits.spot_id WHERE visits.user_id = $1`
 
   return db.query(query, [userID])
     .then(data => data.rows)
