8aa58c0853dedf89a1e61958f97689690ce86472
Janay Ma <janay.ma7@gmail.com>
2023-11-24 13:38:36 -0800

    change visit card to use spot name

--- backend/src/db/queries/01_users.js
+++ backend/src/db/queries/01_users.js
@@ -15,8 +15,10 @@ const getUserByID = (id) => {
 };
 
 const getVisitsByUser = (userID) => {
-  query = `SELECT visits.id as id, visits.time_stamp as date, visits.image_url as image_url, visits.name as name
-   FROM visits WHERE visits.user_id = $1`
+  query = `SELECT spots.name as spot_name, visits.id as id, visits.time_stamp as date, visits.image_url as image_url
+    FROM visits JOIN spots
+    ON spots.id = visits.spot_id
+    WHERE visits.user_id = $1`
 
   return db.query(query, [userID])
     .then(data => data.rows)
