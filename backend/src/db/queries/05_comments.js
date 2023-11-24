b287547402277d2b29a322c5232194a57c7f9719
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 18:20:36 -0800

    feat: add user profile pic to query

--- backend/src/db/queries/05_comments.js
+++ backend/src/db/queries/05_comments.js
@@ -3,7 +3,7 @@ const db = require('../index');
 // Get Comments for one Visit
 const getVisitComments = (id) => {
   return db.query(`
-        SELECT comments.*, users.first_name, users.last_name 
+        SELECT comments.*, users.first_name, users.last_name, users.profile_pic  
         FROM comments 
         JOIN users ON comments.user_id = users.id
         WHERE comments.visit_id = $1;`, [id])
