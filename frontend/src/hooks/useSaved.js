240dd513d224c1b2c0ab560d88b39c1e7c7946a9
Janay Ma <janay.ma7@gmail.com>
2023-11-23 21:04:23 -0800

    cleanup

--- frontend/src/hooks/useSaved.js
+++ frontend/src/hooks/useSaved.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-//checks if spot is saved
-//handles toggling og save button 
 export default function useSaved(userID, spotID) {
 
-  //console.log('in useSaved: ', userID, spotID);
-  //set isSaved stated.
+
   const[ saveID, setSaveID ] = useState(undefined);
 
   //check if save exists if user is logged in;
   useEffect(() => {
-    //post to the checking endpoint
-    //if user is logged in, check 
     if(userID){
-      //console.log(userID)
+
       fetch(`http://localhost:8080/api/saves/checkSave`, {
         method: "POST",
         headers: { 'Content-Type': 'application/json'},
@@ -23,10 +18,8 @@ export default function useSaved(userID, spotID) {
       .then(data => {
         //data.exists <=> save exists! setSaved to true.
         if (data.id) {
-          // console.log("setting save ID as: ", data.id)
           setSaveID(data.id);
         }
-        console.log(data)
       })
       .catch(err => console.log("Error: ", err))  
     } 
@@ -47,23 +40,19 @@ export default function useSaved(userID, spotID) {
       
       //wait for response data
       const data = await res.json();
-      console.log("adding id and Setting save ID to: ", data.id)
       setSaveID(data.id);
 
     } catch (error) {
       console.error('Error: ', error);
       throw error;
     }
-  
   };
   
   const removeSave = async () => {
     
     try {
-      console.log("about to fetch")
       await fetch(`http://localhost:8080/api/saves/${saveID}`, { method: 'DELETE'});
       
-      console.log("deleting id and setting saveID to undefined")
       setSaveID(undefined);
 
     } catch (error) {
@@ -73,17 +62,13 @@ export default function useSaved(userID, spotID) {
   
   const handleSaveClick = function(event, userID, spotID) {
     event.preventDefault();
-    console.log("handle save clicked. Save id is: ", saveID);
-
 
     if (!saveID) {
       addSave(userID, spotID);
     } else {
-      console.log('in handle save, removing save')
       removeSave();
     }
   }
-
 
   return [saveID, handleSaveClick];
 }
