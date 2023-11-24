344e4d7933269bdf62d621b9d97fc31349760eca
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 16:51:33 -0800

    feat: add comment query

--- backend/src/db/queries/05_comments.js
+++ backend/src/db/queries/05_comments.js
@@ -1,3 +1,15 @@
 const db = require('../index');
 
-module.exports = {};
+// Get Comments for one Visit
+const getVisitComments = (id) => {
+  return db.query(`
+        SELECT comments.*, users.first_name, users.last_name 
+        FROM comments 
+        JOIN users ON comments.user_id = users.id
+        WHERE comments.visit_id = $1;`, [id])
+    .then(data => {
+      return data.rows[0];
+    });
+};
+
+module.exports = { getVisitComments };
