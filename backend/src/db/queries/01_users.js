50a32d2526395b8c18073ef54b7a951e3b0348ed
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-24 15:43:02 -0800

    feat: get saves by user query

--- backend/src/db/queries/01_users.js
+++ backend/src/db/queries/01_users.js
@@ -23,7 +23,16 @@ const getVisitsByUser = (userID) => {
     .catch(error => {
       console.error("Error querying the database: ", error)
       throw error});
-}
+};
+
+const getSavesByUser = function(userID) {
+  const query = `SELECT SPOTS.id AS spot_id, SPOTS.lat AS lat, SPOTS.lng AS lng, SPOTS.name AS spotName, SPOTS.city AS city, SPOTS.province AS province, SPOTS.country AS country, VISITS.*
+                            FROM SPOTS
+                            JOIN SAVES ON SPOTS.id = SAVES.spot_id
+                            JOIN VISITS ON SPOTS.id = VISITS.spot_id
+                            WHERE SAVES.user_id = $1;`;
+  return db.query(query, [userID]).then((data) => data.rows);
+};
 
 
-module.exports = { getUsers, getUserByID, getVisitsByUser };
+module.exports = { getUsers, getUserByID, getVisitsByUser, getSavesByUser };
