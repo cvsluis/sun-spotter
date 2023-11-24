2026ad910bce0859f6cfd599f73828174c8305ef
Janay Ma <janay.ma7@gmail.com>
2023-11-24 15:26:55 -0800

    remove visit name from createVisit query

--- backend/src/db/queries/03_visits.js
+++ backend/src/db/queries/03_visits.js
@@ -15,13 +15,13 @@ const getOneVisit = (id) => {
 
 // Create Visit
 const createVisit = (visit) => {
-  const { user_id, spot_id, chosenName, image_url, rating, description, time_stamp } = visit;
+  const { user_id, spot_id, image_url, rating, description, time_stamp } = visit;
 
-  const query = `INSERT INTO visits(user_id, spot_id, name, image_url, rating, description, time_stamp)
-                            VALUES ($1, $2, $3, $4, $5, $6, $7) 
+  const query = `INSERT INTO visits(user_id, spot_id, image_url, rating, description, time_stamp)
+                            VALUES ($1, $2, $3, $4, $5, $6) 
                             RETURNING *;`;
 
-  return db.query(query, [user_id, spot_id, chosenName, image_url, rating, description, time_stamp])
+  return db.query(query, [user_id, spot_id, image_url, rating, description, time_stamp])
     .then(data => {
       return data.rows[0];
     })
