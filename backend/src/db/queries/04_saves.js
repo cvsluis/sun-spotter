b96b3c92d63d18c3e45b0152935a47f695b654a8
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-23 20:30:32 -0800

    Refactor query

--- backend/src/db/queries/04_saves.js
+++ backend/src/db/queries/04_saves.js
@@ -1,7 +1,7 @@
 const db = require("../index");
 
 const getSavedUserSpots = function (userID) {
-  const query = `SELECT SPOTS.*, VISITS.*
+  const query = `SELECT SPOTS.id AS spot_id, SPOTS.lat AS lat, SPOTS.lng AS lng, SPOTS.name AS spotName, SPOTS.city AS city, SPOTS.province AS province, SPOTS.country AS country, VISITS.*
                             FROM SPOTS
                             JOIN SAVES ON SPOTS.id = SAVES.spot_id
                             JOIN VISITS ON SPOTS.id = VISITS.spot_id
