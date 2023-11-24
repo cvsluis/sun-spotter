f6d4e11124a00926382271c51a4355b8db8a2475
Janay Ma <janay.ma7@gmail.com>
2023-11-24 12:45:37 -0800

    add return to map button

--- frontend/src/routes/OneSpot.jsx
+++ frontend/src/routes/OneSpot.jsx
@@ -31,8 +31,21 @@ export default function OneSpot() {
   //get weather info
   const weather = useWeather();
 
+  
+
   return (
     <div className='one-spot'>
+          <Link className='one-spot__btn--add-spot' to='/spots/new'>
+            <span className='one-spot__btn--icons'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-map" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103M10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8zm-6-.8V1.11l-4 .8v12.98z"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+              </svg>
+            </span>
+            <span className='one-spot__btn--text slide-right'>Return to Map</span>
+          </Link>
       <header className='one-spot__header'>
         <OneSpotMap lng={spotInfo.lng} lat={spotInfo.lat} />
         <div>
