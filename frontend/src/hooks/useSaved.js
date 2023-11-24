1f0ccda4b944771c95b125c5ebd801a076e274c2
Janay Ma <janay.ma7@gmail.com>
2023-11-23 17:31:55 -0800

    fetch to checkSave route

--- frontend/src/hooks/useSaved.js
+++ frontend/src/hooks/useSaved.js
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+//checks if spot is saved
+//handles toggling og save button 
 export default function useSaved() {
+
   const[ isSaved, setIsSaved ] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/saves/checkSave`, {
+      method: "POST",
+      headers: 'application/json',
+      body: JSON.stringify({ userID, spotID })
+    })
+    .then((res => res.json()))
+    .then(data => console.log(data))
+    .catch(err => console.log("Error: ", err))
+  })
   
 
   //will need to handle posts to saves once user auth is implemented
--- frontend/src/routes/OneSpot.jsx
+++ frontend/src/routes/OneSpot.jsx
@@ -17,7 +17,17 @@ import '../styles/OneSpot.scss';
 
 export default function OneSpot() {
 
-
+//check if save exists
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/saves/checkSave`, {
+      method: "POST",
+      headers: 'application/json',
+      body: JSON.stringify({ userID, spotID })
+    })
+    .then((res => res.json()))
+    .then(data => console.log(data))
+    .catch(err => console.log("Error: ", err))
+  })
   const [ userID, setUserID ] = useOutletContext();
 
   const spotID = useParams().id;
