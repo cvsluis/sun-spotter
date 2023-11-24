93481fdb660ed17fdc7f5d7e56c87de8473b035a
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-23 18:45:27 -0800

    feat: add comments

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import TimeAgo from 'react-timeago';
 import '../styles/Comment.scss';
 
-export default function Comment() {
+export default function Comment({ comment }) {
   return (
-    <div>
+    <div className='comment__container'>
+      <img src={`http://localhost:8080/${comment.profile_pic}`}></img>
+      <div className='comment__bubble'>
+        <div className='comment__bubble-header'>
+          <h5>{comment.first_name} {comment.last_name}</h5>
+          <p><TimeAgo date={comment.created_at} /></p>
+        </div>
+        <p>{comment.description}</p>
+      </div>
     </div>
   );
 };
--- frontend/src/hooks/useVisitData.js
+++ frontend/src/hooks/useVisitData.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from 'react';
 export default function useVisitData(visitId) {
   const [visit, setVisit] = useState({});
   const [labels, setLabels] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const fetchVisit = async () => {
       try {
         const visit = await fetch(`http://localhost:8080/api/visits/${visitId}`).then(res => res.json());
         const labelList = await fetch(`http://localhost:8080/api/visits/${visitId}/labels`).then(res => res.json());
+        const commentList = await fetch(`http://localhost:8080/api/visits/${visitId}/comments`).then(res => res.json());
 
         setVisit(visit);
         setLabels(labelList);
+        setComments(commentList);
       } catch (err) {
         console.error('Error fetching data', err);
       }
@@ -20,5 +23,5 @@ export default function useVisitData(visitId) {
     fetchVisit();
   }, []);
 
-  return [visit, labels];
+  return [visit, labels, comments];
 }
