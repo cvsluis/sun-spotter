01746c5041f751bfa1c8dc2a580732e50ed8b267
Janay Ma <janay.ma7@gmail.com>
2023-11-24 13:10:48 -0800

    check for cookie in root element

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -3,11 +3,23 @@ import TopNavBar from "../components/TopNavBar";
 import { useState } from "react";
 import React from "react";
 
+import parseCookie from "../utils/parseCookie";
+
 
 
 export default function Root () {
+  console.log(document.cookie)
+  let userIDState = { user_id: undefined};
 
-  const [ userID, setUserID ] = useState(undefined);
+  if(document.cookie) {
+    console.log('parsing cookie')
+    const parsedCookie = parseCookie(document.cookie);
+    userIDState = { user_id: Number(parsedCookie.user_id) };
+  }
+  const [ userID, setUserID ] = useState(userIDState);
+  console.log(userID)
+  
+  
   
   return (
     <div>
