21b028cbc3b9ec16390430fd9c39ef273957d08f
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-23 22:14:02 -0800

    Format

--- backend/src/routes/saves.js
+++ backend/src/routes/saves.js
@@ -1,54 +1,62 @@
 const router = require("express").Router();
-const saveQueries = require('../db/queries/04_saves');
+const saveQueries = require("../db/queries/04_saves");
 
-
-module.exports = db => {
-
-  router.get('/saves/:spots/:id', (req, res) => {
+module.exports = (db) => {
+  router.get("/saves/:spots/:id", (req, res) => {
     const userID = req.params.id;
-  
-    saveQueries.getSavedUserSpots(userID)
-    .then(savedSpots => {
-      res.json(savedSpots);
-    })
-    .catch(err => {
-      res.status(500)
-      .json({error: err.message});
-    });
+
+    saveQueries
+      .getSavedUserSpots(userID)
+      .then((savedSpots) => {
+        res.json(savedSpots);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
-  router.post('/saves', (req, res) => {
+  router.post("/saves", (req, res) => {
     const { userID, spotID } = req.body;
-    saveQueries.createSave(userID, spotID)
+    saveQueries
+      .createSave(userID, spotID)
       .then((result) => {
-        res.status(200).json({ message: "save Created!", id: result.id })
+        res.status(200).json({ message: "save Created!", id: result.id });
       })
-      .catch(err => console.log("Error: ", err));
-  })
-
-  router.post('/saves/checkSave', (req, res) => {
-    const { userID, spotID } = req.body;
-
-    saveQueries.checkSave(userID, spotID) 
-      .then(result => {
-        if (!result) {
-          res.status(200).json({exists: 'false', message: "This location is not saved."})
-        } else {
-          res.status(200).json({exists: 'true', message: "This location is saved!", id: result.id})
-        }   
-      })
-      .catch(err => console.log("Error: ", err))
+      .catch((err) => console.log("Error: ", err));
   });
 
-  router.delete('/saves/:id', (req, res) => {
+  router.post("/saves/checkSave", (req, res) => {
+    const { userID, spotID } = req.body;
+
+    saveQueries
+      .checkSave(userID, spotID)
+      .then((result) => {
+        if (!result) {
+          res
+            .status(200)
+            .json({ exists: "false", message: "This location is not saved." });
+        } else {
+          res
+            .status(200)
+            .json({
+              exists: "true",
+              message: "This location is saved!",
+              id: result.id,
+            });
+        }
+      })
+      .catch((err) => console.log("Error: ", err));
+  });
+
+  router.delete("/saves/:id", (req, res) => {
     const saveID = req.params.id;
 
-    saveQueries.deleteSave(saveID) 
+    saveQueries
+      .deleteSave(saveID)
       .then(() => {
-        res.status(200).json({message: "success deleting!"})
+        res.status(200).json({ message: "success deleting!" });
       })
-      .catch(err => console.log("Error: ", error));
+      .catch((err) => console.log("Error: ", error));
   });
   return router;
-}
-
+};
