4117c45eef51f5b901262539e02e2b833461c377
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-24 16:25:06 -0800

    fix: refactor user spots and visits state

---
+++ frontend/src/hooks/useUserPins.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export default function useUserPins(userId) {
+
+  const [userSaves, setUserSaves] = useState([]);
+  const [userVisits, setUserVisits] = useState([]);
+
+  useEffect(() => {
+    const fetchVisit = async () => {
+      try {
+        const saves = await fetch(`http://localhost:8080/api/users/${userId}/saves`).then(res => res.json());
+        const visits = await fetch(`http://localhost:8080/api/users/${userId}/visits`).then(res => res.json());
+
+        setUserSaves(saves);
+        setUserVisits(visits);
+      } catch (err) {
+        console.error('Error fetching data', err);
+      }
+    };
+
+    fetchVisit();
+  }, []);
+
+  return [userSaves, userVisits];
+}
