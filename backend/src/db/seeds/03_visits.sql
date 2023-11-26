6144c9b77fde9ae0911c26ece8af2f2a0e9a3cb1
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-25 23:16:16 -0800

    fix: rearrange visits data

--- backend/src/db/seeds/03_visits.sql
+++ backend/src/db/seeds/03_visits.sql
@@ -1,18 +1,18 @@
 INSERT INTO VISITS(user_id, spot_id, image_url, rating, description, time_stamp, created_at)
 VALUES
-  (1, 8, 'images/13.jpg', 5, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
-  (1, 5, 'images/6.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
-  (1, 10, 'images/5.jpg', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
-  (2, 1, 'images/3.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
+  (3, 8, 'images/1.jpg', 4, 'Beautiful colors today up on Mount Tolmie :)', '2023-11-14 16:30:00', DEFAULT),
+  (2, 5, 'images/6.jpg', 5, 'Amazing sunset as usual on Moss Rock', '2023-08-04 20:00:00', DEFAULT),
+  (3, 10, 'images/5.jpg', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
+  (1, 1, 'images/3.jpg', 4, 'Gorgeous views but a bit windy!', '2023-05-14 19:30:00', DEFAULT),
   (2, 5, 'images/9.jpg', 5, 'Never a bad sunset here :)', '2023-02-16 17:00:00', DEFAULT),
-  (2, 7, 'images/12.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
+  (3, 7, 'images/12.jpg', 4, 'Beautiful!', '2022-09-23 19:30:00', DEFAULT),
   (3, 3, 'images/11.jpg', 5, 'Amazing light filtering through clouds over the ocean :)', '2022-10-29 18:30:00', DEFAULT),
   (3, 5, 'images/16.jpg', 5, 'Best sunset in Vic!', '2023-03-9 16:30:00', DEFAULT),
-  (3, 2, 'images/18.jpg', 4, 'Always beautiful, sometimes too windy!!', '2023-06-20 18:30:00', DEFAULT),
+  (1, 6, 'images/18.jpg', 4, 'Always beautiful, sometimes too windy!!', '2023-06-20 18:30:00', DEFAULT),
   (1, 9, 'images/15.jpg', 4, 'Beautiful and pink!', '2023-07-26 21:30:00', DEFAULT),
   (2, 7, 'images/17.jpg', 4, 'Gorgeous!', '2023-05-14 19:30:00', DEFAULT),
-  (3, 6, 'images/12.jpg', 4, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
-  (1, 1, 'images/7.jpg', 4, 'Pink Pink PInk!', '2023-05-14 19:30:00', DEFAULT),
-  (3, 10, 'images/13.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT),
+  (2, 2, 'images/13.jpg', 5, 'Perfect sunset vibes!!', '2023-07-26 20:30:00', DEFAULT),
+  (2, 1, 'images/7.jpg', 4, 'Pink Pink PInk!', '2023-05-14 19:30:00', DEFAULT),
+  (1, 10, 'images/19.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT),
   (2, 4, 'images/10.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT),
-  (3, 5, 'images/19.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT);
+  (1, 5, 'images/2.jpg', 4, 'So beatiful here :)', '2023-05-14 19:30:00', DEFAULT);
