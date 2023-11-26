623bcd81b095766cd43401c0a0646eba2900d6f3
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-25 23:31:47 -0800

    feat: update search input

--- frontend/src/components/AllSpotsSearch.jsx
+++ frontend/src/components/AllSpotsSearch.jsx
@@ -4,7 +4,12 @@ import '../styles/AllSpotsSearch.scss';
 export default function AllSpotsSearch({ searchInput, handleSearchInputChange }) {
   return (
     <div className='allSpotsSearch__container'>
-      <input placeholder='Search location' autoComplete='off' value={searchInput} onChange={handleSearchInputChange} onKeyUp={handleSearchInputChange}/>
+      <div className='allSpotsSearch__input'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search allSpotsSearch__icon" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+        </svg>
+        <input placeholder='Search location' autoComplete='off' value={searchInput} onChange={handleSearchInputChange} onKeyUp={handleSearchInputChange} />
+      </div>
       {/* to be implemented */}
       <button id='allspots-btn-filter' className='allSpots-btn-grey'>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
