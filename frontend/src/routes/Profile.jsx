6831c0a350284749be89c4dc1501acead4fd7369
Janay Ma <janay.ma7@gmail.com>
2023-11-26 20:09:41 -0800

    fetch user page based on the route id instead of user cookie

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 
 //import componenets
@@ -13,13 +13,15 @@ export default function Profile() {
   const [user, setUser] = useState();
   const [userID, setUserID] = useOutletContext();
 
+  const userPageID = useParams().id;
+
   useEffect(() => {
     const userIDFromCookie = Cookies.get("user_id");
     setUserID(userIDFromCookie);
   }, []);
 
   const fetchUser = () => {
-    fetch(`http://localhost:8080/api/users/${userID}`)
+    fetch(`http://localhost:8080/api/users/${userPageID}`)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
