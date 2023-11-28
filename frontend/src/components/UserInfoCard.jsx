59721656c9c6f431c2e3b67d47671921e38063a4
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-27 17:30:31 -0800

    Add user info and style

--- frontend/src/components/UserInfoCard.jsx
+++ frontend/src/components/UserInfoCard.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-import "../styles/UserInfoCard.scss"
+import "../styles/UserInfoCard.scss";
 
-export default function UserInfoCard({user}) {
-  
+export default function UserInfoCard({ user }) {
   if (!user) {
     return null;
   }
 
-  const {first_name, last_name, profile_pic, city, province, country} = user;
-    
+  const { first_name, last_name, profile_pic, city, province, country } = user;
 
   return (
-    <div className="user-info__container">
-      <img src={`http://localhost:8080/${profile_pic}`} alt="profile" className="card__image" />
-      <h2 className="card__name">{first_name} {last_name}</h2>
-      <p className="card__location">{city}, {province}</p>
-      <p className='card__location'>{country}</p>
-    </div>
- );
-};
+    <div class="user-info__container">
+      <div class="user-info__picture">
+        <img
+          src={`http://localhost:8080/${profile_pic}`}
+          alt="profile"
+          className="card__image"
+        />
+      </div>
+      <div class="card__info">
+        <h1>{first_name} {last_name}</h1>
+        <p>{city}, {province}</p>
+        <p>{country}</p>
+        </div>
+        <div class="profile-stats">
+          <p>18 Followers</p>
+          <p>2 Stats</p>
+          <p>Lists 18</p>
+          <p>Following 0</p>
+        </div>
+      </div>
 
+  );
+}
