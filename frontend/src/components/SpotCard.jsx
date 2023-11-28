c2e1b50ee5d41ff67b2f26ab53236cce67644117
Janay Ma <janay.ma7@gmail.com>
2023-11-28 11:39:57 -0800

    add save button to spot card

--- frontend/src/components/SpotCard.jsx
+++ frontend/src/components/SpotCard.jsx
@@ -12,10 +12,12 @@ export default function SpotCard({ spot, userID }) {
   const [saveID, handleSaveClick] = useSaved();
 
   return (
-    <>
-    <div className='allSpots_save-btn'>
-      
-    </div>
+    <div className='spotCard'>
+      <button className='spotCard__save-btn'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#F86204" viewBox="0 0 16 16">
+          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
+        </svg>
+      </button>
     <Link to={`/spots/${spot.id}`} className='spotCard__container' id={`spot_card_${spot.id}`}>
       <div className='spotCard__image'>
         <img src={`http://localhost:8080/${spot.image_url}`} />
@@ -48,6 +50,6 @@ export default function SpotCard({ spot, userID }) {
         <h4>{labelList}</h4>
       </div>
     </Link>
-    </>
+    </div>
   );
 }
