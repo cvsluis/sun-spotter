23b23a4e40dc3ce2012784ddc308ad4c22a369e7
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-27 16:53:20 -0800

    Refactor profile

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
+import useUser from "../hooks/useUser";
 
 //import componenets
 import UserInfoCard from "../components/UserInfoCard";
@@ -10,36 +11,16 @@ import UserSpots from "../components/UserSpots";
 import "../styles/Profile.scss";
 
 export default function Profile() {
-  const [user, setUser] = useState();
   const [userID, setUserID] = useOutletContext();
 
   const userPageID = useParams().id;
 
+  const [user] = useUser(userID);
+
   useEffect(() => {
     const userIDFromCookie = Cookies.get("user_id");
     setUserID(userIDFromCookie);
-  }, []);
-
-  const fetchUser = () => {
-    fetch(`http://localhost:8080/api/users/${userPageID}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => setUser(data[0]))
-      .catch((error) => console.error("Error fetching data", error));
-  };
-
-  useEffect(() => {
-    fetchUser();
-  }, []);
-
-
-  // const fetchSavedUserSpots = () => {};
-
-  // const fetchUserVisits = () => {};
+  });
 
   return (
     <div className="profile">
