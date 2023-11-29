66da9f9e8d7ffbd05ea82aef9e7936c72da5736b
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-29 11:47:27 -0800

    Style

--- frontend/src/components/UserInfoCard.jsx
+++ frontend/src/components/UserInfoCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import useUserStats from '../hooks/useUserStats';
+import useUserStats from "../hooks/useUserStats";
 import "../styles/UserInfoCard.scss";
 
 export default function UserInfoCard({ user, userID }) {
-
   const [totalSaves, totalVisits] = useUserStats(userID);
 
   if (!user) {
@@ -13,24 +12,42 @@ export default function UserInfoCard({ user, userID }) {
   const { first_name, last_name, profile_pic, city, province, country } = user;
 
   return (
-    <div class="user-info__container">
-      <div class="user-info__picture">
+    <div className="user-info__container">
+      <div>
         <img
           src={`http://localhost:8080/${profile_pic}`}
           alt="profile"
-          className="card__image"
+          className="user-info__picture"
         />
       </div>
-      <div class="card__info">
-        <h1>{first_name} {last_name}</h1>
-        <p>{city}, {province}</p>
-        <p>{country}</p>
-        </div>
-        <div class="profile-stats">
-          <p>{totalSaves} Saved</p>
-          <p>{totalVisits} Visited</p>
+      <div className="user-info__details">
+        <h1>
+          {first_name} {last_name}
+        </h1>
+        <div className="user-info__location">
+          <p>
+            {city}, {province}
+          </p>
+          <p>{country}</p>
         </div>
       </div>
-
+      <div className="user-info__profile-stats">
+        <h2>My Spots</h2>
+        <div className="user-info__counts">
+        <div className="user-info__saved">
+          <p>
+            <strong>{totalSaves}</strong>
+          </p>
+          <p>Saved</p>
+        </div>
+        <div className="user-info__visits">
+          <p>
+            <strong>{totalVisits}</strong>
+          </p>
+          <p>Visited</p>
+        </div>
+        </div>
+      </div>
+    </div>
   );
 }
