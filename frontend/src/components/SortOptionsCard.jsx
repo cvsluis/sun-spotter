183fdf17c21caca4304075e6ed43a6fb57a3ab86
Janay Ma <janay.ma7@gmail.com>
2023-11-30 02:03:55 -0800

    render radio button options for sorting

--- frontend/src/components/SortOptionsCard.jsx
+++ frontend/src/components/SortOptionsCard.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import '../styles/SortOptionsCard.scss'
 
 export default function SortOptionsCard({ spots, menuOptions, isCheckbox}) {
-  console.log(menuOptions)
   return (
     <ul className="sortOptions">
-     {menuOptions.map(option => (
-        <li>option!!!!</li>
-      ))}
+      <li>
+        <input type="radio" name="sort" value="id" checked />
+        <label for="id">Most Recent</label>
+      </li>
+
+      <li>
+        <input type="radio" name="sort" value="rating_count" checked />
+        <label for="rating_count">Most Visited</label>
+      </li>
+
+      <li>
+        <input type="radio" name="sort" value="rating" checked />
+        <label for="rating">Rating</label>
+      </li>
     </ul>
   )
 }
