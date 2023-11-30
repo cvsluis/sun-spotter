3b4a24b1130d36e3ae920dfb638e12e6276bb3a8
Janay Ma <janay.ma7@gmail.com>
2023-11-30 02:24:04 -0800

    make radio buttons controlled components

--- frontend/src/components/SortOptionsCard.jsx
+++ frontend/src/components/SortOptionsCard.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/SortOptionsCard.scss'
 
 export default function SortOptionsCard({ spots, menuOptions, isCheckbox}) {
+
+  const [ selectedSortOption, setSelectedSortOption ] = useState('id');
+
+  const handleOptionChange = function(event) {
+    setSelectedSortOption(event.target.value)
+  }
+
+  
+
+
   return (
     <ul className="sortOptions">
       <li>
-        <input type="radio" name="sort" value="id" checked />
-        <label for="id">Most Recent</label>
+        <input type="radio" name="sort" value="id" checked={selectedSortOption === 'id'} onChange={handleOptionChange}/>
+        <label htmlFor="id">Most Recent</label>
       </li>
 
       <li>
-        <input type="radio" name="sort" value="rating_count" checked />
-        <label for="rating_count">Most Visited</label>
+        <input type="radio" name="sort" value="rating_count" checked={selectedSortOption === 'rating_count'} onChange={handleOptionChange}/>
+        <label htmlFor="rating_count">Most Visited</label>
       </li>
 
       <li>
-        <input type="radio" name="sort" value="rating" checked />
-        <label for="rating">Rating</label>
+        <input type="radio" name="sort" value="rating" checked={selectedSortOption === 'rating'} onChange={handleOptionChange}/>
+        <label htmlFor="rating">Rating</label>
       </li>
     </ul>
   )
