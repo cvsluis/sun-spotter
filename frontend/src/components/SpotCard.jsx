a910a0d244c1ec5447ad7ec1472ef1f7d135b714
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-29 18:13:37 -0800

    feat: refactor save icon

--- frontend/src/components/SpotCard.jsx
+++ frontend/src/components/SpotCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import '../styles/SpotCard.scss';
 import useSaved from '../hooks/useSaved';
+import SaveEmpty from '../assets/svg/SaveEmpty';
+import SaveFilled from '../assets/svg/SaveFilled'
 
 export default function SpotCard({ spot, userID }) {
   const labelList = spot.list.join(', ');
@@ -16,15 +18,7 @@ export default function SpotCard({ spot, userID }) {
     <div className='spotCard'>
       {userID && 
       <button className='spotCard__save-btn' onClick={(event) => {handleSaveClick(event, userID, spot.id)}}>
-        {saveID ? 
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#F86204" viewBox="0 0 16 16">
-          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
-        </svg>
-        :
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
-        </svg> 
-        }
+        { saveID ? <SaveFilled size={'24px'}/> : <SaveEmpty size={'24px'}/> }
       </button>
       }
     <Link to={`/spots/${spot.id}`} className='spotCard__container' id={`spot_card_${spot.id}`}>
