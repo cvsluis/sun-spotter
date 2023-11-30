3971480f3ad36124af9d7d936f5c1735cd1344ec
Janay Ma <janay.ma7@gmail.com>
2023-11-30 01:31:08 -0800

    render list of search options

--- frontend/src/components/SearchOptionsCard.jsx
+++ frontend/src/components/SearchOptionsCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function SearchOptionsCard() {
-  return <div>hello</div>
+export default function SearchOptionsCard({ spots, menuOptions, isCheckbox}) {
+  console.log(menuOptions)
+  return (
+    <ul className="searchOptions">
+     {menuOptions.map(option => (
+        <li>option</li>
+      ))}
+    </ul>
+  )
 }
