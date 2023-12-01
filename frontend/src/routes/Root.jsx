2bba9d12796207fe1158690c833ee9f152a9983e
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-30 18:18:36 -0800

    feat: redirect root to home after inital render

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import TopNavBar from "../components/TopNavBar";
-import { useState } from "react";
-import React from "react";
 
 import parseCookie from "../utils/parseCookie";
 
 export default function Root () {
+  const navigate = useNavigate();
+
   let userIDState = undefined;
 
   if(document.cookie) {
@@ -17,6 +18,11 @@ export default function Root () {
 
   // search input state to track across home page and all spots page
   const [searchInput, setSearchInput] = useState('');
+
+  // redirect root page to home after initial render
+  useEffect(() => {
+    navigate('/home');
+  }, []);
 
   return (
     <div>
