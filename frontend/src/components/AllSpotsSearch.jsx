c45d9d8a3aaada0522f82b599fac6af6f348775b
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-12-05 11:39:45 -0800

    feat: add delete input on all spots search

--- frontend/src/components/AllSpotsSearch.jsx
+++ frontend/src/components/AllSpotsSearch.jsx
@@ -5,7 +5,7 @@ import useSearchOptions from '../hooks/useSearchOptions';
 import SortOptionsCard from './SortOptionsCard';
 import FilterOptionsCard from './FilterOptionsCard';
 
-export default function AllSpotsSearch({ searchInput, handleSearchInputChange, spots, setSpots }) {
+export default function AllSpotsSearch({ searchInput, handleSearchInputChange, spots, setSpots, clearHomeSearchInput }) {
 
   const [isSearchMenu, isFilterMenu, toggleSearchOptionMenu, useOutsideClick ] = useSearchOptions();
 
@@ -38,6 +38,10 @@ export default function AllSpotsSearch({ searchInput, handleSearchInputChange, s
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search allSpotsSearch__icon" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
+        { searchInput !== '' &&
+        <svg onClick={clearHomeSearchInput} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill allSpotsSearch__delete" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+        </svg> }
         <input placeholder='Search location' autoComplete='off' value={searchInput} onChange={handleSearchInputChange} onKeyUp={handleSearchInputChange} />
       </div>
 
